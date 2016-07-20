0d65a0bf9202ca091445240f36c9a36dbc4f9f4d
Manuel Martinez <manmar92@gmail.com>
2016-07-20 14:06:30 -0500

    save progress

--- sh/r_bitstegnet.sh
+++ sh/r_bitstegnet.sh
@@ -1,6 +1,5 @@
 #! /bin/sh
-deluge-console resume file;
 sudo bin/bitstegnet 1;
-rm mktorrent/file;
-deluge-console pause file;
-deluge-console recheck file;
+sudo rm mktorrent/file;
+sudo deluge-console recheck file;
+sudo killall deluged
